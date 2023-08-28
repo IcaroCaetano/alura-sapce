f2b5d6fa9f8ce0bd5023694bcdeb6fac64e6c33a
IcaroCaetano <icaro.caetano@gmail.com>
2023-08-28 17:48:04 -0300

    feat: incluindo menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './styles/estilosGlobais.scss'
 import Cabecalho from "./componentes/Cabecalho";
+import Menu from './componentes/Menu';
 
 function App() {
   return (
-    <Cabecalho/>
+    <>
+      <Cabecalho/>
+      <Menu/>
+    </>
   );
 }
 
